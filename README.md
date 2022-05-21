3736db6976fc22ee0e90df20b865c3e6dd415510
ryandewillis <dewillisr@yahoo.com>
2022-05-21 15:47:13 -0500

    Rename project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DcacAppShell
+# DCAC Web Client
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/dcac-app-shell'),
+      dir: require('path').join(__dirname, './coverage/dcac-web-client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
